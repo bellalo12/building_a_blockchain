c4b10b653df6a66c0cd65d45e03827dd6e9a61d6
Bella Lo <bellalo@BestaMacBook-Air.local>
2018-05-21 21:19:30 +0700

    creating new block and new transaction

---
+++ dev/test.js
@@ -0,0 +1,28 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+const previousBlockHash = 'OIE09866JNM93';
+const currentBlockData = [
+  {
+    amount: 10,
+    sender: 'NJE29403JHN3M',
+    recipient: '03842DYCS56'
+  },
+  {
+    amount: 300,
+    sender: 'LHHIO3850J3L0',
+    recipient: '249UDLE03DRY'
+  },
+  {
+    amount: 50,
+    sender: '3F3NEFHY26093',
+    recipient: '220IEL394FE'
+  }
+];
+
+const nonce = 100;
+
+
+
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce))
