2aa5f42c3a0a9732ce51b40f7e0990ccff39f82f
Bella Lo <bellalo@BestaMacBook-Air.local>
2018-05-26 12:45:29 +0800

    building POST/transaction

--- dev/api.js
+++ dev/api.js
@@ -8,14 +8,18 @@ const bitcoin = new Blockchain();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+//get entire blockchain
 app.get('/blockchain', (req, res)=>{
   res.send(bitcoin);
 });
 
+//create a new transaction
 app.post('/transaction', (req, res)=>{
-  
+  const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient)
+  res.json({note:`Transaction will be added in block ${blockIndex}`})
 });
 
+//mine a block
 app.get('/mine', (req, res)=>{
 
 });
